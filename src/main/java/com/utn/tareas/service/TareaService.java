d1fad5ca972ecb375ae1648a8fe08773c4c04dc8
Mariano <marianom.lopez@alumnos.frm.utn.edu.ar>
2025-11-06 20:32:30 -0300

    Hecha la Parte 4

--- src/main/java/com/utn/tareas/service/TareaService.java
+++ src/main/java/com/utn/tareas/service/TareaService.java
@@ -3,6 +3,7 @@ package com.utn.tareas.service;
 import com.utn.tareas.model.Prioridad;
 import com.utn.tareas.model.Tarea;
 import com.utn.tareas.repository.TareaRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,7 +12,16 @@ import java.util.stream.Collectors;
 
 @Service
 
+
 public class TareaService {
+    @Value("${app.nombre}")
+    private String nombreApp;
+
+    @Value("${app.max-tareas:100}")
+    private int maxTareas;
+
+    @Value("${app.mostrar-estadisticas: true}")
+    private boolean mostrarEstadisticas;
 
     private final TareaRepository tareaRepository;
 
@@ -20,9 +30,20 @@ public class TareaService {
     }
 
     public void agregarTarea (String descripcion, Prioridad prioridad){
-        com.utn.tareas.model.Tarea nuevaTarea = new Tarea(0L, descripcion, false, prioridad);
+        if (tareaRepository.findAll().size() < maxTareas) {
+            com.utn.tareas.model.Tarea nuevaTarea = new Tarea(0L, descripcion, false, prioridad);
 
-        tareaRepository.guardar(nuevaTarea);
+            tareaRepository.guardar(nuevaTarea);
+        } else {
+            System.out.println("Error: límite máximo de tareas: "+maxTareas);
+        }
+
+    }
+
+    public void imprimirConfiguracion(){
+        System.out.println("Nombre de la aplicación: "+nombreApp);
+        System.out.println("Máximo de tareas en lista: "+maxTareas);
+        System.out.println("¿Se muestran las estadisticas?: "+mostrarEstadisticas);
     }
 
     public List<Tarea> listarTodas (){
